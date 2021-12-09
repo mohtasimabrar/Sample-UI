f634cf7573694563de0f261be600e8973f599d7e
Mohtasim Abrar Samin <mohtasim.abrar@gmail.com>
2021-12-09 21:16:08 +0600

    Profile page added

--- Sample UI/TabControllers/ProfileViewController.swift
+++ Sample UI/TabControllers/ProfileViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,6 +17,17 @@ class ProfileViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(signOut))
         // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        image.layer.borderWidth = 1.0
+        image.layer.masksToBounds = false
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.cornerRadius = image.frame.size.width / 2
+        image.clipsToBounds = true
     }
     
     @objc func signOut(){
